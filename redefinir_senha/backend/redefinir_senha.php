2d91bc75f5ed3fd77ef66cc2169505207d76414e
samueldevcorebiz <samuel.rocha@corebiz.ag>
2021-08-29 02:46:58 -0300

    Criação do frontend de redefinição de senha

---
+++ redefinir_senha/backend/redefinir_senha.php
@@ -0,0 +1,72 @@
+<?php
+
+function turnPageToHttps() {
+	if (isset($_SERVER['HTTPS']) &&
+        ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') 
+    {
+    // ESTÁ EM HTTPS
+    }
+    else 
+    {
+        $protocol = "https://";
+        $actual_link = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+        header("Location:$protocol$actual_link");
+    }
+}
+
+function redefinePassword() {
+
+    $senha_gerada = "gotech@".random_int(100, 999);
+    $BODY_REQUEST =  "{cd:".$_REQUEST["cd"].", id:'".$_REQUEST["id"]."', password:'".sha1($senha_gerada)."'}";
+    $URL = 'https://smartrecorder-api.herokuapp.com/pessoa/redefinir_senha';
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $URL,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => 'UTF-8',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $BODY_REQUEST,
+        CURLOPT_HTTPHEADER => array(
+        "Cache-Control: no-cache",
+        "Content-Type: application/json"
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+
+        die("<script>alert(Ocorreu um erro ao alterar a senha)</script>");
+        
+    } else {
+        
+        $array = array();
+        $redefinir_password_response = json_decode($response);
+        $redefinir_password_response['senha'] = $senha_gerada;
+
+        if($redefinir_password_response != "" && $redefinir_password_response != null){
+        if(property_exists($redefinir_password_response, "status")){
+            if($redefinir_password_response->{'status'} == 0){  
+                return $redefinir_password_response;
+            }else{
+                die("<script>alert(Ocorreu um erro ao alterar a senha)</script>");
+            }
+        }else{
+            die("<script>alert(Ocorreu um erro ao alterar a senha)</script>");
+        }
+        }else{
+            die("<script>alert(Ocorreu um erro ao alterar a senha)</script>");
+        } 
+    }
+}
+
---
+++ redefinir_senha/index.php
@@ -0,0 +1,44 @@
+<?php 
+    include "backend/redefinir_senha.php";
+	  turnPageToHttps();
+
+    $response = redefinePassword();
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Redefinição de Senha - Sem Parar</title>
+    
+    <!-- Favicon -->
+    <!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/logo-new-brand-144x144.png"/>
+    <link rel="icon" type="image/png" sizes="144x144" href="assets/img/logo-new-brand-144x144.png"/>
+    <link rel="icon" type="image/png" sizes="72x72" href="assets/img/logo-new-brand-72x72.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo-new-brand-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo-new-brand-16x16.png"/> -->
+
+    <!-- Metas -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/redenir_senha.css" />
+  </head>
+
+  <body ga-ev-label="login_popup">
+    <section class="container-redefinir-senha">
+        <div class="box-redefinir-senha">
+            <img class="logo-gotech" src="assets/img/logo-gotech.png"/><br/><br/>
+            Olá <b><?php $response->{'nome'}?></b>,<br/><br/>
+            Sua senha foi redefinida para <b><?php $response->{'senha'}?></b><br/><br/>
+            Agora você já pode voltar ao app e ter o controle do seu consumo de energia na palma da sua mão. 😉<br/><br/>
+            Conte conosco!<br/><br/>
+            Abraços,<br/>
+            Time GoTech
+        </div>
+    </section>
+  </body>
+</html>

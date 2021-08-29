1ceecfb2d4248b49d84127979b7075424d2d7a39
samueldevcorebiz <samuel.rocha@corebiz.ag>
2021-08-29 02:58:38 -0300

    depurando erros

--- redefinir_senha/index.php
+++ redefinir_senha/index.php
@@ -1,4 +1,10 @@
 <?php 
+    /* Informa o nível dos erros que serão exibidos */
+    error_reporting(E_ALL);
+  
+    /* Habilita a exibição de erros */
+    ini_set("display_errors", 1);
+    
     include "backend/redefinir_senha.php";
 	  turnPageToHttps();
 
